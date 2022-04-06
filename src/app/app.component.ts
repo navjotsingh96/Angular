eeeb213dcce984eb72773fbdd7c1a62a2764ec88
navjotsingh96 <navjot.singh96@web.de>
2022-04-06 12:57:06 +0200

    change image in slideshow function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Singh';
+export class AppComponent implements OnInit {
+
+  images = ['header_new.jpg', '2.jpg', 'pexels-andrea-piacquadio-3764580.jpg'];
+  currentImage = 0;
+
+
+  ngOnInit() {
+    this.uploadImage();
+  }
+
+  uploadImage() {
+    setInterval(() => {
+      this.currentImage = this.currentImage % this.images.length;
+      this.currentImage++;
+    },8000);
+  }
 }
