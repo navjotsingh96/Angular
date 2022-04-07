35e419987211e68eb7dbf3cacf4381bf3fdcd68f
navjotsingh96 <navjot.singh96@web.de>
2022-04-07 10:54:33 +0200

    animated text headlines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   images = ['header_new.jpg', '2.jpg', 'pexels-andrea-piacquadio-3764580.jpg'];
+  headlines = [
+    'Bring engernnierng to next level',
+    'Born to Code',
+    'Studied in India'
+  ];
+
   currentImage = 0;
   showImage = true;
 
@@ -26,6 +32,6 @@ export class AppComponent implements OnInit {
       setTimeout(() => {
         this.showImage = true;
       }, 10);
-    },8000);
+    }, 8000);
   }
 }
